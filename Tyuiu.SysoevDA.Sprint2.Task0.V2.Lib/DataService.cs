e9c512ec2195523e860d708e899f0feddcacccd2
SysoevDA <fuzzii2739@gmail.com>
2023-10-10 12:32:06 +0500

    Add project files.

---
+++ Tyuiu.SysoevDA.Sprint2.Task0.V2.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.SysoevDA.Sprint2.Task0.V2.Lib
+{
+    public class DataService : ISprint2Task0V2
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x + 1 == y;
+            res[1] = x != y + 1;
+            res[2] = x < y;
+            res[3] = x > y - 1;
+            res[4] = x + 1 <= y;
+            res[5] = x >= y;
+
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.SysoevDA.Sprint2.Task0.V2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SysoevDA.Sprint2.Task0.V2.Lib;
+
+namespace Tyuiu.SysoevDA.Sprint2.Task0.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Сысоев Д.А | ПКТБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Сысоев Данил Алексеевич | ПКТБ-23-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
+            Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
+            Console.WriteLine("* (False, True, False, True, False, True), при x = 123, y = 123           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int x = 123;
+            int y = 123;
+            Console.WriteLine("X =  " + x);
+            Console.WriteLine("Y =  " + y);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            for (int i=0; i<6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
