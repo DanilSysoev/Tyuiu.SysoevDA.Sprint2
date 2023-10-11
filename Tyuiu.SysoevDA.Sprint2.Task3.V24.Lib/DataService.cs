f5263809a03b1b3588998e8fe690a6cb2a1f9116
SysoevDA <fuzzii2739@gmail.com>
2023-10-11 21:10:44 +0500

    Task3

---
+++ Tyuiu.SysoevDA.Sprint2.Task3.V24.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SysoevDA.Sprint2.Task3.V24.Lib
+{
+    public class DataService : ISprint2Task3V24
+    {
+        public double Calculate(double x)
+        {
+            double res;
+            if (x == 0)
+            {
+                res = (Math.Cos(x) + (12 / Math.Pow(x, 2)));
+            }
+            else if (x > 0)
+            {
+                res = (x * Math.Pow((10 + Math.Sin(Math.Sqrt(x + 1))/ x),x));
+            }
+            else if ((x < 0 ) && (x > -28))
+            {
+                res = (Math.Pow((1 + (1 / Math.Pow(x, 2))),x));
+            }
+            else
+            {
+                res = (Math.Pow(x, 2) + 10 * x - (1 / x));
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.SysoevDA.Sprint2.Task3.V24.Test/DataServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SysoevDA.Sprint2.Task3.V24.Lib;
+
+namespace Tyuiu.SysoevDA.Sprint2.Task3.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(10.988, res);
+        }
+        [TestMethod]
+        public void ValidCalculate2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(Math.Cos(x) + (12 / Math.Pow(x, 2)), res);
+        }
+        [TestMethod]
+        public void ValidCalculate3()
+        {
+            DataService ds = new DataService();
+            double x = -20;
+
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(0.951, res);
+        }
+        [TestMethod]
+        public void ValidCalculate4()
+        {
+            DataService ds = new DataService();
+            double x = -50;
+
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(2000.02, res);
+        }
+    }
+}
